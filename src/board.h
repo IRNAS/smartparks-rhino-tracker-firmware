e88116b8173e1ba342f7f23025775ba287ae8f37
musti <musti@wlan-si.net>
2019-06-10 09:55:13 +0200

    minor fix

--- src/board.h
+++ src/board.h
@@ -3,7 +3,7 @@
 
 #define GPS_EN PA8
 #define GPS_BCK PB15
-#define BOOST_EN PA5 - not defined by default
+#define BOOST_EN PA5
 #define LED_RED 16 PA0
 #define BAN_MON_EN PB12
 #define BAN_MON 0 PA3
