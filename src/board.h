2ac2670e4ffdf7fafdd523edb316ccef3785db17
musti <musti@wlan-si.net>
2019-10-03 08:51:51 +0200

    External voltage monitoring

--- src/board.h
+++ src/board.h
@@ -1,7 +1,23 @@
 #ifndef BOARD_H_
 #define BOARD_H_
 
-#define VER2_2
+#define VER2_3
+
+// lion
+#ifdef VER2_3
+    #define PIN_REED PH0
+    #define GPS_EN PB14
+    #define LIGHT_EN PB15
+    #define BAN_MON_EN PB12
+    #define BAN_MON PA4
+    #define GPS_BCK PA8
+    #define PIN_ADC PA5
+    #define LED_RED PA0
+    #define A_INT1 PB2
+    #define A_INT2 PB7
+    #define VSWR_EN PB13
+    #define VSWR_ADC PA3
+#endif
 
 #ifdef VER2_2
     #define PIN_REED PH0
