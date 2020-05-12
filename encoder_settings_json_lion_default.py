c6e495547dd83a08a71612207707faa868e09e4b
musti <musti@wlan-si.net>
2020-05-12 15:56:18 +0200

    Better default values

--- encoder_settings_json_lion_default.py
+++ encoder_settings_json_lion_default.py
@@ -1,7 +1,7 @@
 import json
 
 data = {}
-data['system_status_interval'] = 5
+data['system_status_interval'] = 30
 
 system_functions = {}
 system_functions['accelerometer_enabled'] = True
@@ -12,35 +12,35 @@ system_functions['charging_enabled'] = True
 data['system_functions'] = system_functions
 
 lorawan_datarate_adr = {}
-lorawan_datarate_adr["datarate"]=1
+lorawan_datarate_adr["datarate"]=2
 lorawan_datarate_adr["confirmed_uplink"]=False
 lorawan_datarate_adr["adr"]=False
 data['lorawan_datarate_adr'] = lorawan_datarate_adr
 
-data['gps_periodic_interval'] =10
-data['gps_triggered_interval'] =1
-data['gps_triggered_threshold'] =10
-data['gps_triggered_duration'] =10
-data['gps_cold_fix_timeout'] =200
-data['gps_hot_fix_timeout'] =60
-data['gps_min_fix_time'] =5
-data['gps_min_ehpe'] =50
+data['gps_periodic_interval'] = 15
+data['gps_triggered_interval'] = 0
+data['gps_triggered_threshold'] = 10
+data['gps_triggered_duration'] = 10
+data['gps_cold_fix_timeout'] = 200
+data['gps_hot_fix_timeout'] = 60
+data['gps_min_fix_time'] = 5
+data['gps_min_ehpe'] = 50
 data['gps_hot_fix_retry'] = 5
-data['gps_cold_fix_retry'] =255
-data['gps_fail_retry'] =0
+data['gps_cold_fix_retry'] = 2
+data['gps_fail_retry'] = 0
 
 gps_settings = {}
 gps_settings['d3_fix'] = True
-gps_settings['fail_backoff'] = True
+gps_settings['fail_backoff'] = False
 gps_settings['hot_fix'] = True
 gps_settings['fully_resolved'] = False
 
 data['gps_settings'] = gps_settings
-data['system_voltage_interval'] =30
+data['system_voltage_interval'] = 30
 data['gps_charge_min'] = 3000
-data['system_charge_min'] =3900
-data['system_charge_max'] =4000
-data['system_input_charge_min'] =10000
+data['system_charge_min'] = 3900
+data['system_charge_max'] = 4100
+data['system_input_charge_min'] = 10000
 
 json_data = json.dumps(data)
 print(json_data)
